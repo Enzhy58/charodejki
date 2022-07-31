6ce274235f48c10987f9e042c997c9a9c72f9c96
Enzhy58 <ang.borisova2015@yandex.ru>
2022-07-31 10:46:46 +0300

    added adaptive home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-
+  $('.menu__open').on('click', function(){
+    $('.menu__box').addClass('menu__box--active');
+    $('.wrapper').addClass('wrapper__fixed');
+  });
+  $('.menu__close').on('click', function () {
+    $('.menu__box').removeClass('menu__box--active');
+    $('.wrapper').removeClass('wrapper__fixed');
+  });
 });
