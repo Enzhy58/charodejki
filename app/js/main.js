cd82e6c545c62427303a06ec950b228471ca836f
Enzhy58 <ang.borisova2015@yandex.ru>
2023-10-17 19:09:37 +0300

    added adaptive header

--- app/js/main.js
+++ app/js/main.js
@@ -15,3 +15,30 @@ const addIngredients = checkboxes => {
 }
 
 addIngredients(inputsCheckbox);
+
+$(function(){
+    $('.menu__open').on('click', function(){
+      $('.menu__box').addClass('menu__box--active');
+      $('.wrapper').addClass('wrapper__fixed');
+    });
+    $('.menu__close, .menu__link').on('click', function () {
+      $('.menu__box').removeClass('menu__box--active');
+      $('.wrapper').removeClass('wrapper__fixed');
+    });
+    $('.menu__link, .btn').on('click', function (e) {
+      e.preventDefault();
+      var id = $(this).attr('href'), top = $(id).offset().top;
+      $('body, html').animate({ scrollTop: top }, 1500);
+    });
+  });
+  
+  document.getElementById('home').onclick = function () {
+    window.location.href = 'index.html';
+  };
+  document.getElementById('cakes').onclick = function () {
+    window.location.href = 'cakes.html';
+  };
+  document.getElementById('desserts').onclick = function () {
+    window.location.href = 'desserts.html';
+  };
+  
